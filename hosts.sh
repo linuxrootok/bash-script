63fc327df13cef0dc52608c1d5c8b4be61ee05a1
Tyler <givemealetter@gmail.com>
2014-09-19 13:35:13 +0800

    fix tmp path

--- hosts.sh
+++ hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/bin/wget http://www.360kb.com/kb/2_122.html -O /root/software/hosts.txt &> /dev/null
+/bin/wget http://www.360kb.com/kb/2_122.html -O /tmp/hosts.txt &> /dev/null
 /bin/cat /etc/hosts0 > /etc/hosts2
-/bin/sed -n 's/^<span>\([^#]*\)<span>\(.*\)<\/span>\(.*\)<\/span><br \/>$/\1\2\3/gp' /root/software/hosts.txt >> /etc/hosts2
+/bin/sed -n 's/^<span>\([^#]*\)<span>\(.*\)<\/span>\(.*\)<\/span><br \/>$/\1\2\3/gp' /tmp/hosts.txt >> /etc/hosts2
 /bin/mv /etc/hosts /etc/hosts1
 /bin/mv /etc/hosts2 /etc/hosts
